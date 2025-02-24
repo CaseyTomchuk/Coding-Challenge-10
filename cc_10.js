9eec8123b6fb3ffff241407a35461accae696c59
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-02-23 21:22:19 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -9,7 +9,7 @@ class Product {
     };
 
     getDetails() {
-        console.log(`Product: ${this.name}, ID: ${this.id}, Price: ${this.price}, Stock: ${this.stock}`); // logging the properties for any given object of the product class
+        console.log(`Product: ${this.name}, ID: ${this.id}, Price: $${this.price}, Stock: ${this.stock}`); // logging the properties for any given object of the product class
     }
 
     updateStock(quantity) {
@@ -22,3 +22,22 @@ product1.getDetails();  // Expected output: "Product: Laptop, ID: 101, Price: $1
 product1.updateStock(3); // 3 orders are made, so we have 3 less laptops, so we are at 7
 product1.getDetails(); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
 
+// Task 2: Creating an order class
+
+class Order {
+    constructor(orderID, product, quantity) {
+        this.orderID = orderID;
+        this.product = product;
+        this.quantity = quantity;
+        this.product.updateStock(this.quantity); // whenever an order is created, we are reducing the stock by the quantity of the order
+    }
+
+    getOrderDetails() {
+        let totalPrice = this.product.price * this.quantity;
+        return `Order ID: ${this.orderID}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${totalPrice}`;
+    }
+}
+
+let order1 = new Order (501, product1, 2);
+console.log(order1.getOrderDetails()); // Expected output: Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400
+product1.getDetails(); // Expected output: Product: Laptop, ID: 101, Price: $1200, Stock: 5
